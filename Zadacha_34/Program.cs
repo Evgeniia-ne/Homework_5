5b7a1355b688fa9670e4dbce31131c85856f10f5
Asus <koncarik@mail.ru>
2022-12-20 01:15:36 +0500

    Количество четных чисел в массиве из трехзначных чисел

---
+++ Zadacha_34/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+const int SIZE = 7;
+const int LEFT_RANGE = 100;
+const int RIGHT_RANGE =999;
+
+int[] array = new int[SIZE];
+
+
+int[] FillArray(int size, int leftRange, int rightRange)
+{
+    Random rand = new Random();
+    int[] arr = new int[size];
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rand.Next(leftRange, rightRange + 1);
+    }
+    return arr;
+}
+
+array = FillArray(SIZE, LEFT_RANGE, RIGHT_RANGE);
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+if (array[i] % 2 == 0)
+count++;
+
+System.Console.WriteLine('[' + string.Join(", ", array) + ']');
+System.Console.WriteLine("Количество четных чисел в массиве: " + count);
+
+// void FillArrayRandomNumbers(int[] numbers)
+// {
+//     for(int i = 0; i < numbers.Length; i++)
+//     {
+//         numbers[i] = new Random().Next(100,1000);
+//     }
+// }
+// void PrintArray(int[] numbers)
+// {
+//     Console.Write("[ ");
+//     for(int i = 0; i < numbers.Length; i++)
+//     {
+//         Console.Write(numbers[i] + " ");
+//     }
+//     Console.Write("]");
+//     Console.WriteLine();
+// }
